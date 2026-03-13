71e8b455214e4454d08687d02d1f2171fa3f611f
jumpon097 <38902090+jumpon097@users.noreply.github.com>
2026-03-13 12:34:42 +0700

    Add files via upload

---
+++ wards/sattabongkot/sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'pharmacom-sattabongkot-v1';
+const URLS_TO_CACHE = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
